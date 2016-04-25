c0534f853ba39c91e003653e56285703ce0d8fe1
David Walker <david.walker@anaplan.com>
2016-04-25 10:52:33 +0100

    adding delays to filesystem functions

--- git.py
+++ git.py
@@ -1,21 +1,25 @@
 import logging
-
+from time import sleep
 
 def get_commit(hook_data):
     commit_hash = hook_data['after']
     cmd = "git pull "+commit_hash
     print cmd
+    sleep(1)
     branch = get_branch_from_push(hook_data)
     cmd = "git checkout " + branch
     print cmd
+    sleep(1)
     return
 
 def pull(commit_hash):
     cmd = "git pull " + commit_hash
     print cmd
+    sleep(1)
     return
 
 def checkout(branch):
     cmd = "git checkout " + branch
     print cmd
+    sleep(1)
     return
--- knife.py
+++ knife.py
@@ -1,6 +1,7 @@
 import logging
-
+from time import sleep
 
 def run(k_cmd):
     print k_cmd
+    sleep(1)
     return
